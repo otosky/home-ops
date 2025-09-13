056d3bcf0b57aa7ae9f8e2c2b71e8a63e5fcea9e
Oliver Tosky <olivertosky@gmail.com>
2025-09-12 21:16:29 -0700

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "otosky Nix Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          makejinja
+          python313
+          cilium-cli
+          cloudflared
+          fluxcd
+          talhelper
+          cue
+
+          sops
+          ssh-to-age
+          gnupg
+          age
+          jq
+          yq-go
+          kubectl
+          kubernetes-helm
+          helmfile
+
+          just
+          go-task
+          kustomize
+          talosctl
+          kubeconform
+        ];
+      };
+    });
+}
